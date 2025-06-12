d246beb7934ee7833be95335822e8d4e2ef3d642
Gabriel Willian <168204456+gabrielwilliandev@users.noreply.github.com>
2025-06-12 10:26:48 -0300

    Renomeando arquivos

--- TowerHanoi.c
+++ TowerHanoi.c
@@ -1,68 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista.h"
+#include "discos.h"
+#include "historico.h"
 
-// Estrutura - pilha (torre)
-typedef struct Node {
-    int disco;
-    struct Node* prox;
-} Node;
-
-typedef struct {
-    Node* topo;
-} Pilha;
-
-
-// Funções p/ manipulação da pilha
-void inicializar_pilha(Pilha* pilha) {
-    pilha->topo = NULL;
-}
-
-void DestruirLista(Lista **Lref) {
-    Lista *L = *Lref;
-    No *p = L->inicio;
-    while (p != NULL) {
-        No *aux = p;
-        p = p->prox;
-        free(aux);
-    }
-    free(L);
-    *Lref = NULL;
-}
-
-int pop(Pilha* pilha) {
-    if (!pilha->topo) return -1;
-    Node* temp = pilha->topo;
-    int disco = temp->disco;
-    pilha->topo = temp->prox;
-    free(temp);
-    return disco;
-}
-
-// Conter disco em uma torre
-int contar_discos(Pilha* pilha) {
-    int count = 0;
-    Node* atual = pilha->topo;
-    while (atual) {
-        count++;
-        atual = atual->prox;
-    }
-    return count;
-}
-
-// Obter disco em uma posição específica
-int obter_disco(Pilha* pilha, int pos) {
-    Node* atual = pilha->topo;
-    for (int i = 0; i < pos && atual; i++) {
-        atual = atual->prox;
-    }
-    return atual ? atual->disco : 0;
-}
-
-Lista* historico = NULL;
 
 // Menu principal
 int main() {
+    Lista* historico = NULL;
     historico = CriarLista();
     carregar_historico(historico);
     int opcao;
@@ -74,7 +18,7 @@ int main() {
         scanf("%d", &opcao);
         switch (opcao) {
             case 1:
-                jogar(historico);
+                //jogar(historico);
                 break;
             case 2:
                 exibir_historico(historico);
