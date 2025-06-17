e69879b2132d589a17aa9789a5aac386c76736aa
Bianca Maria <bianca20062406@gmail.com>
2025-06-16 22:26:19 -0300

    Corrigindo erros ortográficos

--- TowerHanoi.c
+++ TowerHanoi.c
@@ -13,7 +13,7 @@ int main() {
     char busca[50];
     do {
         printf("\n=== Torre de Hanoi ===\n");
-        printf("1. Jogar\n2. Exibir historico\n3. Buscar por nome\n4. Buscar por data\n5. Ajuda\n6.Sair\n");
+        printf("1. Jogar\n2. Exibir historico\n3. Buscar por nome\n4. Buscar por data\n5. Ajuda\n6. Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
         switch (opcao) {
